7c5c5aa4f47cbac06589e42653335bf66e828e40
Tanjim Chowdhury <tanjim@live.co.uk>
2023-01-24 20:56:44 +0000

    added match and feature dfs

--- leaderboard_reader.py
+++ leaderboard_reader.py
@@ -10,6 +10,7 @@ that is needed to feed into the ML model.
 
 
 import cv2
+import pickle
 import numpy as np
 import pandas as pd
 import pytesseract
@@ -264,3 +265,109 @@ def create_input(img_path):
     # now begin creating input dataframe for ML model
     
     
+    time = get_time(hsv_img)
+    
+    if len(time) == 0:
+        # load linear regression time model
+        model_filename = "time_estimator.sav"
+        time_linear_regressor = pickle.load(open(filename, 'rb'))
+        time_input = read_df.drop["Time"]   # check input matches
+        time = time_linear_regressor.predict(time_input)
+    else:
+        # insert time parse from min:sec to sec
+        # assume here that its "M:SS"
+        time_list = re.findall(r"\d{2}", time)
+        time = int(time_list[0]) * 60 + int(time_list[1])
+    
+    # create input df that will have the features ready for the PCA model
+    input_df = read_df[
+        ["Round", "Time"]
+    ].copy()
+    working_df = pd.DataFrame()
+    
+    for player_number in range(1, 11):
+    
+        # rounds
+        working_df[f"player{player_number}_k_per_round"] = (
+            read_df[f"player{player_number}_kills"] / read_df["Round"]
+        )
+        read_df[f"player{player_number}_a_per_round"] = (
+            read_df[f"player{player_number}_assists"] / read_df["Round"]
+        )
+        read_df[f"player{player_number}_d_per_round"] = (
+            read_df[f"player{player_number}_deaths"] / read_df["Round"]
+        )
+    
+        # winning rounds (score)
+        if player_number <= 5:
+            score_ = read_df["team1_score"]
+        else:
+            score_ = read_df["team2_score"]
+        
+        # if no rounds won, avoid a zero division by setting won rounds to 1
+        score_[score_ == 0] = 1
+    
+        working_df[f"player{player_number}_k_per_w_round"] = read_df[f"player{player_number}_kills"] / score_
+        working_df[f"player{player_number}_a_per_w_round"] = read_df[f"player{player_number}_assists"] / score_
+        working_df[f"player{player_number}_d_per_w_round"] = read_df[f"player{player_number}_deaths"] / score_
+    
+    
+    for metric in ["k", "d", "a"]:
+        for round_ind in ["", "w_"]:
+            # all players for each team
+            # team1
+            input_df[f"team1_min_{metric}_per_{round_ind}r_of_total_p"] = working_df[
+                [f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(1, 6)]
+            ].min(axis=1)
+            input_df[f"team1_mean_{metric}_per_{round_ind}r_of_total_p"] = working_df[
+                [f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(1, 6)]
+            ].mean(axis=1)
+            input_df[f"team1_max_{metric}_per_{round_ind}r_of_total_p"] = working_df[
+                [f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(1, 6)]
+            ].max(axis=1)
+            # team2
+            input_df[f"team2_min_{metric}_per_{round_ind}r_of_total_p"] = working_df[
+                [f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(6, 11)]
+            ].min(axis=1)
+            input_df[f"team2_mean_{metric}_per_{round_ind}r_of_total_p"] = working_df[
+                [f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(6, 11)]
+            ].mean(axis=1)
+            input_df[f"team2_max_{metric}_per_{round_ind}r_of_total_p"] = working_df[
+                [f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(6, 11)]
+            ].max(axis=1)
+    
+            # alive players for each team
+            # logic for alive players is to just multiply
+            # team 1
+            input_df[f"team1_min_{metric}_per_{round_ind}r_of_alive_p"] = (
+                working_df[[f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(1, 6)]]
+                .multiply(np.array(read_df[[f"player{player_number}_alive" for player_number in range(1, 6)]]))
+                .min(axis=1)
+            )
+            input_df[f"team1_mean_{metric}_per_{round_ind}r_of_alive_p"] = (
+                working_df[[f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(1, 6)]]
+                .multiply(np.array(read_df[[f"player{player_number}_alive" for player_number in range(1, 6)]]))
+                .mean(axis=1)
+            )
+            input_df[f"team1_max_{metric}_per_{round_ind}r_of_alive_p"] = (
+                working_df[[f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(1, 6)]]
+                .multiply(np.array(read_df[[f"player{player_number}_alive" for player_number in range(1, 6)]]))
+                .max(axis=1)
+            )
+    
+            # team 2
+            input_df[f"team2_min_{metric}_per_{round_ind}r_of_alive_p"] = (
+                working_df[[f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(6, 11)]]
+                .multiply(np.array(read_df[[f"player{player_number}_alive" for player_number in range(6, 11)]]))
+                .min(axis=1)
+            )
+            input_df[f"team2_mean_{metric}_per_{round_ind}r_of_alive_p"] = (
+                working_df[[f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(6, 11)]]
+                .multiply(np.array(read_df[[f"player{player_number}_alive" for player_number in range(6, 11)]]))
+                .mean(axis=1)
+            )
+            input_df[f"team2_max_{metric}_per_{round_ind}r_of_alive_p"] = (
+                working_df[[f"player{player_number}_{metric}_per_{round_ind}round" for player_number in range(6, 11)]]
+                .multiply(np.array(read_df[[f"player{player_number}_alive" for player_number in range(6, 11)]]))
+                .max(axis=1)
+            )
