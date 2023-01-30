b027163e5755c33477ac7531e7fa512391664da8
Chowdhury <tanjim.chowdhury@ralphlauren.com>
2023-01-30 19:12:55 +0000

    trained time estimator

--- ML_models/time_estimator.py
+++ ML_models/time_estimator.py
@@ -13,8 +13,9 @@ the other features as time is only known to the player sometimes (before bomb
 import pickle
 import pandas as pd
 import git
+import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
 
 
 # load in the features dataframe
@@ -22,14 +23,18 @@ repo = git.Repo(".", search_parent_directories=True)
 data = pd.read_pickle(repo.working_tree_dir + "/features_dfs/features_df.pkl")
 
 # remove some columns
-data = data.drop(["match_ID", "team1_won_round", "team2_won_round"], axis=1)
+#data = data.drop(["match_ID", "team1_won_round", "team2_won_round"], axis=1)
+corrmat = data.corr()
+target = data.Time
+data = data[["team1_players_alive", "team2_players_alive"]]
+
 # Split the data set into training and test splits (90/10 split used)
 x_train, x_test, y_train, y_test = train_test_split(
-    data.drop(["Time"], axis=1), data.Time, test_size=0.10, random_state=0
+    data, target, test_size=0.10, random_state=0
 )
 
 # Make linear regression model instance
-linear_regression = Ridge()
+linear_regression = LinearRegression()
 
 # fit to the data
 linear_regression.fit(x_train, y_train)
@@ -38,5 +43,5 @@ linear_regression.fit(x_train, y_train)
 score = linear_regression.score(x_test, y_test)
 
 # save the model
-# filename = "time_estimator.sav"
-# pickle.dump(linear_regression, open(filename, "wb"))
+filename = "time_estimator.sav"
+pickle.dump(linear_regression, open(filename, "wb"))
