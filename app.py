2e2525de0af2673640bf33454bb6634690f73805
Chowdhury <tanjim.chowdhury@ralphlauren.com>
2023-02-14 17:10:30 +0000

    variable webpage

--- app.py
+++ app.py
@@ -6,17 +6,28 @@ Created on Thu Feb  9 10:10:36 2023
 
 Flask web application that will display results of predictor
 
+
+Used https://blog.miguelgrinberg.com/post/dynamically-update-your-flask-web-pages-using-turbo-flask
+for turbo-flask implementation
+
+
 """
 
 from flask import Flask, render_template
-
+import random
 
 
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
+@app.context_processor
+def inject_winner_and_probability():
+    winner = random.choice(["team1", "team2"])
+    probability = random.random()*50+50
+    return {"winner":winner,"probability":probability}
