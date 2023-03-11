a3fddb10476fe083051538831fe451e88824566b
Tanjim Chowdhury <tanjim@live.co.uk>
2023-03-11 11:46:44 +0000

    added & fixed sounds

--- app.py
+++ app.py
@@ -20,9 +20,7 @@ import threading
 from path_to_prediction import write_prediction, get_latest_file, obtain_prediction
 import webbrowser
 import pandas as pd
-from playsound import playsound
 import winsound
-from pathlib import Path
 
 
 # initialise web app
@@ -39,9 +37,8 @@ PCA = pickle.load(open(repo.working_tree_dir + "/ML_models/PCA.sav", "rb"))
 xgbTree = pickle.load(open(repo.working_tree_dir + "/ML_models/xgbTree.sav", "rb"))
 
 # set audio paths 
-t_side_path = repo.working_tree_dir + "/sounds/t_side.mp3"
-t_side_path = Path().cwd() / (repo.working_tree_dir + "/sounds/t_side.mp3")
-ct_side_path = Path(repo.working_tree_dir + "/sounds/ct_side.mp3")
+ct_path = repo.working_tree_dir + "\\sounds\\ct_side.wav"
+t_path = repo.working_tree_dir + "\\sounds\\t_side.wav"
 
 # load in image directory path
 with open("path.txt", "r") as file:
@@ -53,23 +50,28 @@ write_prediction("None")
 print("ML Model Initialised")
 
 
-def update_prediction(image_directory_path, previous_file, latest_file, PCA, scaler, xgbTree):
+def update_prediction(image_directory_path, PCA, scaler, xgbTree):
     """
     Updates the ML model result in the text file if a new leaderboard image
     file is found
     """
-
+    
+    global previous_file
+    global latest_file
+    
     latest_file = get_latest_file(image_directory_path)
 
     if latest_file != previous_file:
+        print("New file detected:", latest_file, "compared to", previous_file)
         previous_file = latest_file
         outcome = obtain_prediction(latest_file, PCA, scaler, xgbTree)
         write_prediction(outcome)
-        print()
+        print("New file detected:", latest_file, "compared to", previous_file)
+        return (1)
 
     else:
         print("No new file")
-        pass
+        return(0)
 
 
 @app.route("/")
@@ -96,7 +98,7 @@ def inject_winner_and_probability():
     Pulls the winner and probability from text file
     """
 
-    update_prediction(image_directory_path, previous_file, latest_file, PCA, scaler, xgbTree)
+    update = update_prediction(image_directory_path, PCA, scaler, xgbTree)
 
     dict_keys = ["round", "winner", "probability"]
     dict_output = ["...", "...", "..."]
@@ -112,11 +114,13 @@ def inject_winner_and_probability():
     if tree_outcome[0] >= tree_outcome[1]:
         dict_output[1] = "T"
         dict_output[2] = tree_outcome[0]
-        playsound(r"t_side_path")
+        if update:
+            winsound.PlaySound(t_path, 0)
     else:
         dict_output[1] = "CT"
         dict_output[2] = tree_outcome[1]
-        playsound(ct_side_path)
+        if update:
+            winsound.PlaySound(ct_path, 0)
         
     dict_output[0] = tree_outcome[2]
     dict_output[2] = int(dict_output[2] * 100)
